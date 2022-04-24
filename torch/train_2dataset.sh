d31c6165c4dcde18cc28cada9672546262406cad
quantumiracle <zhding@mail.ustc.edu.cn>
2022-04-24 15:59:39 -0400

    r

--- torch/train_2dataset.sh
+++ torch/train_2dataset.sh
@@ -7,7 +7,7 @@ echo "Save as: " $DATE
 # nohup python train.py --id $DATE > log/$DATE$RAND.log &
 
 # declare -a data=('500' '1000' '1500' '2000' '5000')
-declare -a data=('1000')
+declare -a data=('100' '300' '500' '700')
 
 for i in ${data[@]}; do
     echo $i
