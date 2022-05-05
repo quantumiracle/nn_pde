53d57e9abab6f3eb6b52a3bf87b918eb8c896eed
quantumiracle <zhding@mail.ustc.edu.cn>
2022-05-04 22:08:18 -0400

    r

--- torch/train_heat.sh
+++ torch/train_heat.sh
@@ -6,10 +6,11 @@ echo "Save as: " $DATE
 
 # nohup python train.py --id $DATE > log/$DATE$RAND.log &
 
-declare -a data=('100')
-declare -a dim=('32' '64' '128' '256' '512')
+# declare -a data=('100')
+# declare -a dim=('32' '64' '128' '256' '512')
 
-# declare -a data=('100' '300' '500' '700' '900')
+declare -a dim=('20')
+declare -a data=('1100' '1300' '1500' '1700' '1900')
 
 for i in ${data[@]}; do
     for j in ${dim[@]}; do
